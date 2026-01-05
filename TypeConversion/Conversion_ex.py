4c8bac831e9a8e1d1ceee53ab8c5b25915cd7395
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-05 09:34:52 +0530

    Create Conversion_ex.py for type conversion examples
    Add examples of type conversion in Python.

---
+++ TypeConversion/Conversion_ex.py
@@ -0,0 +1,22 @@
+#Type Conversion is nothing but converting one data type value into another data type value
+a = "12345789"
+b = int(a)
+print(b)
+#This can covert the string of numbers into integer of numbers 
+a = 1985.54
+b = int(a)
+print(b)
+#It converts float to integers
+a = 21
+b = float(a)
+print(b)
+#Converts int to float
+a = 354
+b = str(a)
+print(b)
+#converts int to str 
+
+#So this are the examples of type conversions in python...🙌
+
+
+
