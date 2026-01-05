49516724750c110c79722af148ffd5bf2cfe270c
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-05 20:29:16 +0530

    Add basic rectangle and division calculation scripts
    Added three Python scripts for area and perimeter calculation of a rectangle, and division of two numbers. These scripts take user input, perform the calculation, and print the result.

---
+++ TypeConversion/Coding Practice/Area_of_rectangle.py
@@ -0,0 +1,4 @@
+length = int(input())
+breadth = int(input())
+Area = length * breadth
+print(Area)
---
+++ TypeConversion/Coding Practice/Perimeter_of_rectangle.py
@@ -0,0 +1,4 @@
+lenght = int(input())
+breadth = int(input())
+Perimeter_of_rectangle = 2*(lenght + breadth)
+print(Perimeter_of_rectangle)
