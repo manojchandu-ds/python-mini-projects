97ab5f5032ad1a34c1b90c6bc1acdf386c36462f
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-06 10:06:51 +0530

    Create relational_operator_examples.py with examples
    Add examples and explanations for relational operators.

---
+++ Realtional Operators/relational_operator_examples.py
@@ -0,0 +1,19 @@
+#relational operators are also know as comparision operators 
+#Basic equality
+print(10 == 10.0) #True
+#String Comparison
+print("apple" > "banana")#False
+#Boolean
+print(True == 1) #True
+#Chained relation
+print(5 < 10 > 7)#True
+#Checking that int is equal to str
+print(5 != "5")#True
+
+#Here are some basics of relational operators
+# "==" equals to 
+# "!==" not equals to 
+# "<" less than
+# ">" Greater than 
+# "<=" less than or equals
+# ">=" Greater than or equals 
