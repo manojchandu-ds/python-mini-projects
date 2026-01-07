beb5c0f1f6eb0fc702ac7701e3a960015f1ce80d
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-07 21:59:11 +0530

    Implement comparison of first and last characters
    Add a program to compare first and last characters of input string.

---
+++ Realtional Operators/comparision.py
@@ -0,0 +1,7 @@
+a = input()
+b = a[0]
+c = a[-1]
+print(b == c)
+# This program will check that the given first and last index letters are same or not 
+# for example "label" Here l = 0 and l = - 1 as per index 
+# so True
