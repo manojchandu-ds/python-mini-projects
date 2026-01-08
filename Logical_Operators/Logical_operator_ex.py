e3b9f3267dbe1d10cb14920720d1a5bc4c184a8d
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-08 22:57:06 +0530

    Implement logical operators examples in Python
    Added examples demonstrating the use of logical operators in Python.

---
+++ Logical_Operators/Logical_operator_ex.py
@@ -0,0 +1,19 @@
+#The logical operators are used to perform logical operations on boolean value
+# They are 
+# and,or,not 
+
+#AND operator
+print(True and True) #Tree
+# Here "and" is useful when the two boolean values are true 
+print(True and False) # False
+
+#OR operator
+print(True or False) #True
+print(False or False) #False
+#It satisfies when there is atleast one condition is true
+
+#NOT operator
+print(not(False)) #True
+print(not(True)) #False
+# Logical NOT Operator gives the opposite value
+
