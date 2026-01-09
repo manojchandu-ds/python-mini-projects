67396715a4739c37a5bb234f6f9b9cdc62d8c0c4
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-09 23:21:16 +0530

    Add conditional statements example in Python

---
+++ Conditional_Statement/Conditional_statement.py
@@ -0,0 +1,20 @@
+condition_is_true = True
+if condition_is_true:
+  print("If condtion is true. It will print")
+else:
+  print("Yeah this is else")
+
+#condtional statment 
+
+condition_is_false = False
+condition_is_true = True
+if condition_is_true:
+  print("True")
+elif condition_is_false:
+  print("False")
+else:
+  print("If elif also not true then it comes here")
+  
+  
+#elif aslo know as if else 
+# condtional statements is nothing but either true or false
