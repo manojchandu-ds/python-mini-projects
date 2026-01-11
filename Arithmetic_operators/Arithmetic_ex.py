f8b4deded6cbe70cc8726d0081c462a7fc15fd99
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-11 20:52:16 +0530

    Add arithmetic operations in Arithmetic_ex.py
    Implemented basic arithmetic operations in Python.

---
+++ Arithmetic_operators/Arithmetic_ex.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+print(a + b) #addition   
+print(a - b) #substraction 
+print(a // b) #division withiut floating points 
+print(a / b) # division with floating points( ex 0.0939)
+print(a % b) #modulo (remainder)
+print(a * b) # multiplication 
+print(a ** b) # squaring 
+# these are some arithmetic operators in python
+# there are some built in functions in python like math which we can use "import" to make arithmetic operations easily 
