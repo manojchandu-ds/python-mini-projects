ffb9e60cc2185da582451ea12c69089cd8535c68
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-11 22:03:04 +0530

    Create Nested_conditions_ex.py with examples
    Add example of nested conditions and elif statements.

---
+++ Nested_condtions-&-loops/Nested_conditions_ex.py
@@ -0,0 +1,18 @@
+#nested conditions are nothing but a condition inside an condition 
+# And its syntax be like 
+"""
+if Condition True:
+        Block of code_1
+        if Condition True:
+            Block of code_2
+        Block of code_3
+  Block of code_4
+ """
+#elif statement 
+# we can use elif statements if there are multiple conditions 
+# syntax
+"""
+if condtion:
+elif condition:
+else :  
+"""
