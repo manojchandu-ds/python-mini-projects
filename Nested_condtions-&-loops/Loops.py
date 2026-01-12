d9671ebec8ac046b84b858966d2c8d2190be2e3c
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-12 21:33:16 +0530

    Create Loops.py
    Explained about for loop and while loop

---
+++ Nested_condtions-&-loops/Loops.py
@@ -0,0 +1,21 @@
+#loops are two types one is while loop and another one is for loop in python 
+
+#loops are useful to repeat a code several time 
+#first lets get in to the while loop
+i = 0  #initilization
+while i <= 3: #Termination_condition (loops until the condition is true)
+  print(i)  # A piece of code
+  i = i + 1  # updation (if u miss this, then welocome to the looop that never ends)
+
+#for loop 
+for i in range(1,10):  #we use range function in for loop to print number (1,10) prints number from 1 to 9 
+  print(i)
+# we can also use for loop to itetate over a string by taking each character
+words = manoj
+for word in words:
+  print(word)
+  # this code will iterate through each character in a string words "manoj"....
+
+
+
+  
