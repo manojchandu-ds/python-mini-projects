363be992fef8433ea25856ad2f88032372894b53
Manoj Yeduvaka <manojyeduvaka@gmail.com>
2026-01-14 21:24:18 +0530

    Create for_loopex.py

---
+++ Nested_condtions-&-loops/for_loopex.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(1,n+1):
+  if i % 2 == 0:
+    print(i)
+
+#this will print even numbers from 1 to n 
+
+for i in range(1,n+1):
+  if i % 2 != 0:
+    print(i)
+ #print odds   
+  
